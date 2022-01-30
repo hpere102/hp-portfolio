77f9c8778346060d7dd5c1bc67f3034c458cbacd
danny p <danny@dannys-MacBook-Pro.local>
2022-01-30 14:02:22 -0500

    bootstrap

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function Home() {
     return (
-      <section className="my-5">
+      <section className="title-home">
         <h1 id="home">This is the home page</h1>
+  <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+  <hr class="my-4"></hr>
+  <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+  <p class="lead">
+    <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+  </p>
       </section>
     );
   }
--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -6,8 +6,10 @@ function Work({ currentCategory }) {
   const { name, description } = currentCategory;
   return (
     <section>
+      <div class="work-cont">
       <h1 data-testid="h1tag">{capitalizeFirstLetter(name)}</h1>
       <p>{description}</p>
+      </div>
       <Projects category={currentCategory.name} />
     </section>
   );
