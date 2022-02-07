8570ef0c9bef24d01b9f4971fa8c49674a0ddada
danny p <danny@dannys-MacBook-Pro.local>
2022-02-07 15:44:24 -0500

    new route path

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import Resume from './components/Resume';
 import {
   BrowserRouter as Router,
   Routes,
-  Route
+  Route,
+  Redirect
 }  from "react-router-dom";
 
 
@@ -40,7 +41,10 @@ function App() {
         <Route exact path='/resume' element={<Resume/>}>
         </Route>
         <Route exact path='/contact' element={<ContactForm/>}>
-        </Route>    
+        </Route>   
+
+        <Route path="*" element={<Home/>} />
+
      </Routes>  
      <Footer />   
     </Router>
